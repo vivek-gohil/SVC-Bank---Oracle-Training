f58f5044b061650a13e92ed72bb91b892bdda74c
vivek-gohil <59043694+vivek-gohil@users.noreply.github.com>
2022-04-20 11:46:01 +0530

    Files

---
+++ oracle-sample-database/ot_create_user.sql
@@ -0,0 +1,6 @@
+
+-- create new user
+CREATE USER OT IDENTIFIED BY yourpassword;
+
+-- grant priviledges
+GRANT CONNECT, RESOURCE, DBA TO OT;
---
+++ oracle-sample-database/ot_drop.sql
@@ -0,0 +1,12 @@
+DROP TABLE ot.order_items;  
+DROP TABLE ot.orders;
+DROP TABLE ot.inventories;
+DROP TABLE ot.products;
+DROP TABLE ot.product_categories;
+DROP TABLE ot.warehouses;
+DROP TABLE ot.employees;
+DROP TABLE ot.contacts;
+DROP TABLE ot.customers;
+DROP TABLE ot.locations;
+DROP TABLE ot.countries;
+DROP TABLE ot.regions;
